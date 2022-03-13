b5ad2d448a111f6c18d9ad6bcd8dc682179e797d
littlereed <lookluwei@gmail.com>
2022-03-13 18:24:26 +0800

    message

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['links.papareact.com', 'fakestoreapi.com'],
+  },
+
+  env: {
+    stripe_public_key: process.env.STRIPE_PUBLIC_KEY
+  }
+  
 }
+//stripe public key 需要通过这个配置文件 is exposed to website
+//process这一行通过.env起作用
